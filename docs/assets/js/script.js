895ca99cf0386b271b8573677aa8f053ea3bde06
Taofeek <taofeekolamilekan218@gmail.com>
2020-05-01 00:24:22 +0100

    updated

---
+++ docs/assets/js/script.js
@@ -0,0 +1,40 @@
+(function() {
+
+	$(document).ready(function(e) {
+      
+      	setTimeout(function() {
+        		$("#overlay").slideUp('slow', () => {
+        		$("#wrapper").fadeIn();
+         	});
+        }, 5500)
+
+
+      	$('a').click(function(e) {
+        	if (this.hash !== "") {
+          		e.preventDefault();
+
+          		var hash = this.hash;
+          		$('html, body').animate({ scrollTop: $(hash).offset().top }, 800, function(e) {
+            		window.location.hash = hash
+         	 	})
+        	}
+      	})
+    })
+
+
+    window.onscroll = function() {
+
+	    var nav = document.getElementById('navbar');
+	    var hero = document.getElementById("hero");
+	    var offset = hero.offsetTop;
+	    var width = $(window).width();
+
+	    if (window.pageYOffset > (offset - 200) && width <= 1200) {
+	         nav.classList.add("sticky")
+	    }
+	    else {
+	        nav.classList.remove("sticky")
+	    }
+   	}
+
+})()
