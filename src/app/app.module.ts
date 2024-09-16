390e1fc7a2e1da70a7c19f0c65ed99661b610b6c
Wahab Taofeek <taofeekolamilekan218@yahoo.com>
2024-09-16 17:08:48 +0100

    Feat: Layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+    ],
+    bootstrap: [AppComponent], 
+    imports: [
+        BrowserModule,
+        AboutComponent,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ], 
+    providers: [provideHttpClient(withInterceptorsFromDi())] 
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,8 +4,6 @@ import Typed from 'typed.js';
 import { PasswordValidator } from '../mustMatch'
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 
-declare var $: any;
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,65 +12,96 @@ declare var $: any;
 
 export class HomeComponent implements OnInit {
 
+  submitted = false;
+  selectedTab = 'tao';
+  contactForm: FormGroup;
+
   mail = "Taofeekolamilekan218@gmail.com";
-  
-    contactForm;
-    submitted = false;
 
-  	constructor(private builder: FormBuilder, private http: HttpClient) {
-      this.contactForm = this.builder.group({
-        name: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        message: ['', Validators.required]
-      })
+  tabs = [
+    {
+      name: 'Tao',
+      icon: '',
+      image: '/assets/images/logo.png'
+    },
+    {
+      name: 'About',
+      icon: 'fa-solid fa-address-card',
+      image: ''
+    },
+    {
+      name: 'Skills',
+      icon: 'fa-solid fa-code',
+      image: ''
+    },
+    {
+      name: 'Projects',
+      icon: 'fa-solid fa-diagram-project',
+      image: ''
+    },
+    {
+      name: 'Contact',
+      icon: 'fas fa-phone',
+      image: ''
+    },
+    {
+      name: 'Tutorials',
+      icon: 'fas fa-book',
+      image: ''
+    }
+  ]
+  constructor(
+    private http: HttpClient,
+    private builder: FormBuilder) {
+    this.contactForm = this.builder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    })
+  }
+
+  ngOnInit() {
+    const options = {
+      strings: ['Web Developer', 'Mobile App Developer', 'Solution Developer', 'Consultant and Mentor'],
+      typeSpeed: 50,
+      backSpeed: 50,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true
     }
 
-  	ngOnInit() {
-  		const options = {
-  			strings: ['Web Developer', 'Mobile App Developer', 'Solution Developer', 'Consultant and Mentor'],
-  			typeSpeed: 50,
-  			backSpeed: 50,
-  			showCursor: true,
-  			cursorChar: '|',
-  			loop: true
-  		}
-
-  		const typed = new Typed('.element', options);
-
-
-      const options1 = {
-        strings: ['Welcome To: ', 'TaoCoder'],
-        typeSpeed: 100,
-        backSpeed: 0,
-        showCursor: true,
-        smartBackspace: true,
-        cursorChar: '*',
-        loop: false
-      }
-
-      new Typed('.welcome', options1);
-  	}
-
-    get f() { return this.contactForm.controls; }
-
-    sendMessage(data) {
-
-      this.submitted = true;
-      if (this.contactForm.invalid) 
-        return;
-
-      if (this.contactForm.valid) {
-
-        $("#emailModal").modal('show');
-
-        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-        this.http.post('https://formspree.io/xknvoeny',
-          { name: data.name, replyto: data.email, message: data.message },
-          { 'headers': headers }).subscribe(response => {
-              // console.log(response);
-              $("#status").html("Email Succesfully sent.")
-            }
-          );
-      }
+    // const typed = new Typed('.element', options);
+    const options1 = {
+      strings: ['Welcome To: ', 'TaoCoder'],
+      typeSpeed: 100,
+      backSpeed: 0,
+      showCursor: true,
+      smartBackspace: true,
+      cursorChar: '*',
+      loop: false
     }
+
+    // new Typed('.welcome', options1);
+  }
+
+  get f() { return this.contactForm.controls; }
+
+  sendMessage(data) {
+
+    this.submitted = true;
+    if (this.contactForm.invalid) 
+      return;
+
+    if (this.contactForm.valid) {
+      // $("#emailModal").modal('show');
+      // const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      // this.http.post('https://formspree.io/xknvoeny',
+      //   { name: data.name, replyto: data.email, message: data.message },
+      //   { 'headers': headers }).subscribe(response => {
+      //       // console.log(response);
+      //       $("#status").html("Email Succesfully sent.")
+      //     }
+      //   );
+    }
+  }
 }
